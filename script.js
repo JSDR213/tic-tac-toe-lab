498de8d687fe43ddf6f4792a7e4c20aecbb78f85
nnguyen <nnguyen@visionweb.com>
2023-03-29 17:14:08 -0500

    first commit

--- script.js
+++ script.js
@@ -2,14 +2,19 @@
 
 //remember to Whiteboard and Pseduocode before diving into the actual code!
 
-
 //getting DOM elements
-
+let squares = document.querySelectorAll('.square')
+console.log(squares)
+let score = document.querySelectorAll('.score-row')
+console.log(score)
 
 //win conditions
 
-
 //game logic
-
+squares.forEach((squareButton) => {
+  squareButton.addEventListener('click', function (event) {
+    console.log('X')
+  })
+})
 
 //what else will you need?
