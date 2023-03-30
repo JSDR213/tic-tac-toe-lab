e5631bb5473f19c49bd3e798cbf242da71ffd27d
Carolina Molk <carolinamolk@gmail.com>
2023-03-29 18:34:33 -0600

    done with basic assignment + added reset score button

--- script.js
+++ script.js
@@ -1,34 +1,66 @@
-// TO DO:
-    //add win to score board
-    //if board is full and no one won, register draw 
-    //clear screen to restart game
+// **** TO DO ****:
+//find deadends
+//display nice winning message
+//add best of 3 toggle
 
-
-
-turn = 0
+let turn = 0
 let playerXpositions = []
 let playerOpositions = []
-
+        
 const square = document.querySelectorAll(".square")
+const restartbtn = document.querySelector(".restart")
+const resetbtn = document.querySelector(".reset")
 const xScore = document.querySelector("#playerXScore")
 const oScore = document.querySelector("#playerOScore")
+const ties = document.querySelector("#ties")
+    
+const winCombos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    
+//function that clears game-board
+const restart = () => {
+    square.forEach((b) => b.innerText = "")
+    turn = 0
+    playerOpositions = []
+    playerXpositions = []
+}
 
+//function that clears game-board and resets score
+const reset = () => {
+    restart
+    xScore.innerText = 0
+    oScore.innerText = 0
+    ties.innerText = 0
+}
 
 //function takes player positions as parameter and checks for winning combinations
-function checkForWin(positions) {
-    console.log("Function being called", positions)
-    const winCombos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
-
+const checkForWin = (positions, player) => {
     const isSubset = (array1, array2) => 
         array2.every((element) => array1.includes(element))
 
     for (i = 0; i < winCombos.length; i ++) {
         if(isSubset(positions, winCombos[i])){
-            alert("You Won!")
+            alert(`${player} Won!`)
+            addToScoreboard(player)
+            restart()
         }
     }
 }
 
+//function that adds to scoreboard
+const addToScoreboard = (player) => {
+    if (player == "X"){
+        xScore.innerText = Number(xScore.innerText) + 1
+    }
+    else if (player == "O"){
+        oScore.innerText = Number(oScore.innerText) + 1}
+}
+
+
+//add event listener to Restart button
+restartbtn.addEventListener('click', restart)
+
+//add event listener to Reset button
+resetbtn.addEventListener('click', reset)
 
 //add event listeners to each square
 square.forEach((b) => {b.addEventListener('click', (Event) => {
@@ -47,17 +79,21 @@ square.forEach((b) => {b.addEventListener('click', (Event) => {
         if (turn % 2 == 0){
             b.innerText = ("X")
             playerXpositions.push(sqIndex)
-            checkForWin(playerXpositions)
+            checkForWin(playerXpositions, "X")
         }
         else {
             b.innerText = ("O")
             playerOpositions.push(sqIndex)
-            checkForWin(playerOpositions)
-        }
+            checkForWin(playerOpositions, "O")
+            }
 
         //switch turns
         turn +=1
+        }
+
+    if ((playerXpositions.length + playerOpositions.length) == 9){
+        ties.innerText = Number(ties.innerText) + 1
+        restart()
     }
-})})
 
-
+    })})
